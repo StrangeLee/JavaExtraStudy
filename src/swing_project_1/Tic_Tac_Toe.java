4842db8838d63826cf27c2ee62d142070c70ecbb
LeeSangHa <38284431+StrangeLee@users.noreply.github.com>
2019-12-03 12:03:01 +0900

    [add]

--- src/swing_project_1/Tic_Tac_Toe.java
+++ src/swing_project_1/Tic_Tac_Toe.java
@@ -4,14 +4,15 @@ import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import javax.swing.border.LineBorder;
 import java.awt.*;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.awt.event.*;
+
+/**
+ * 친구들끼리 짜투리 시간때 간간이 하던
+ * 3*3 삼목게임을 java swing 으로 구현해 보았습니다.
+ */
 
 public class Tic_Tac_Toe extends JFrame {
-
-    SimpleDateFormat sdf = new SimpleDateFormat("hh:mm");
+    public static final Font BASIC_FONT = new Font("맑은 고딕", 0 , 15);
 
     JButton [] jbt;
     JLabel winnerLabel = new JLabel("Winner is...");
@@ -19,8 +20,8 @@ public class Tic_Tac_Toe extends JFrame {
     JLabel timeLabel = new JLabel();
 
     Player player = Player.X;
-    boolean gaming = true;
     String [][] board = new String[3][3];
+    boolean gaming = true;
     double gameTime = 0;
 
     public Tic_Tac_Toe() {
@@ -38,11 +39,8 @@ public class Tic_Tac_Toe extends JFrame {
         cp.setBorder(new EmptyBorder(5, 5, 5, 5));
         jbt = new JButton[9];
         for (int i = 0; i < jbt.length; i++) {
-            cp.add(jbt[i] = new JButton());
-            jbt[i].setBackground(Color.white);
-            jbt[i].setBorder(new LineBorder(Color.black));
             int position = i;
-            jbt[i].addActionListener(e -> clickBoard(this.player, position));
+            cp.add(jbt[i] = buttonAction("", 20, e -> clickBoard(this.player, position)));
         }
 
         // board 초기화
@@ -57,6 +55,8 @@ public class Tic_Tac_Toe extends JFrame {
         var sp_np = new JPanel(new FlowLayout());
         var sp_sp = new JPanel(new FlowLayout());
 
+        playerLabel.setFont(BASIC_FONT);
+        winnerLabel.setFont(BASIC_FONT);
         sp_np.add(playerLabel);
         sp_sp.add(winnerLabel);
 
@@ -66,19 +66,14 @@ public class Tic_Tac_Toe extends JFrame {
         add(np, "North");
         add(cp);
         add(sp, "South");
-        
+
         new Thread(() -> gameStart()).start();
+        
         addWindowListener(new WindowAdapter() {
             @Override
             public void windowClosing(WindowEvent e) {
                 super.windowClosing(e);
-                for (int i = 0; i < 3; i++) {
-                    for (int j = 0; j < 3; j++) {
-                        System.out.print(board[i][j] + " ");
-                    }
-                    System.out.println();
-                }
-                Thread.interrupted();
+
             }
         });
     }
@@ -96,7 +91,6 @@ public class Tic_Tac_Toe extends JFrame {
                     return;
                 }
 
-                timeLabel.setText(sdf.format(new Date((long) gameTime)));
             } catch (Exception e) {
                 e.printStackTrace();
             }
@@ -113,6 +107,8 @@ public class Tic_Tac_Toe extends JFrame {
         jbt[position].setEnabled(false);
         board[position / 3][position % 3] = String.valueOf(player);
         winningRule(position, player.getPlayer());
+        playerLabel.setText(this.player.toString() + "'s Turn");
+        revalidate();
     }
 
     private void winningRule(int position, String player) {
@@ -129,6 +125,11 @@ public class Tic_Tac_Toe extends JFrame {
         && board[1][1].equals(player)
         && board[2][0].equals(player))) {
                 JOptionPane.showMessageDialog(null, player + " 승리");
+                winnerLabel.setText("Winner is " + player);
+            for (int i = 0; i < jbt.length; i++) {
+                jbt[i].setEnabled(false);
+            }
+            revalidate();
             return;
         } else {
             for (int i = 0; i < 3; i++) {
@@ -140,6 +141,33 @@ public class Tic_Tac_Toe extends JFrame {
         }
 
     }
+
+    private JButton buttonAction(String name, int fontSize, ActionListener action) {
+        var jb = new JButton(name);
+        jb.setFont(new Font("맑은 고딕", 1, fontSize));
+        jb.addActionListener(action);
+        jb.setBackground(Color.white);
+        jb.setBorder(new LineBorder(Color.black));
+
+        jb.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                super.mouseEntered(e);
+                jb.setBackground(Color.black);
+                jb.setForeground(Color.white);
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                super.mouseExited(e);
+                jb.setBackground(Color.white);
+                jb.setForeground(Color.black);
+            }
+        });
+
+        return jb;
+    }
+
 
     public static void main(String[] args) {
         System.out.println("--------------------------");
